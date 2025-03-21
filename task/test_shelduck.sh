6900935f1c41af543770eceadce09b25df19c26f
Alexander Legeyda <alex@legeyda.com>
2025-03-21 13:50:07 +0300

    refactoring

---
+++ task/test_shelduck.sh
@@ -0,0 +1,23 @@
+
+shelduck import https://raw.githubusercontent.com/legeyda/bobshell/refs/heads/unstable/assert.sh
+shelduck import ../hoid.sh
+
+
+test_run() {
+
+	# shellcheck disable=SC2016
+	hoid shelduck run 'val:
+
+
+shelduck import https://raw.githubusercontent.com/legeyda/bobshell/refs/heads/unstable/string.sh
+x=xyz
+x=$(bobshell_replace "$x" x 1)
+x=$(bobshell_replace "$x" y 2)
+x=$(bobshell_replace "$x" z 3)
+bobshell_die "DEBUG: $x"
+printf %s "$x"
+'
+
+}
+
+
